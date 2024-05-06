ed46eaec3e4cb20d6c3794f5082365df3a917dbe
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-06 17:42:40 +0300

    Added structure for Todo items

---
+++ todo.go
@@ -0,0 +1,26 @@
+package TODOList_REST
+
+type TODOList struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UserList struct {
+	Id     int
+	UserId int
+	ListId int
+}
+
+type TODOItem struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+type ListsItem struct {
+	Id     int
+	ListId int
+	ItemId int
+}
