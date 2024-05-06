dc37f8c4994391d5852ec0fed2f17e0578bd1754
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-06 17:17:12 +0300

    Created struct and base start/stop methods for http server

---
+++ server.go
@@ -0,0 +1,28 @@
+package TODOList_REST
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		MaxHeaderBytes: 1 << 20, //1MB
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	// под капотом запускает бесконечный цикл for
+	//  который слушает все обращения к серверу
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
