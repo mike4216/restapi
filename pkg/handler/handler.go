23d580932fdf1aa62fed8dd3512712a78865312e
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-07 12:49:29 +0300

    FIXED: changed items routes as sub routes for list

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -24,15 +24,15 @@ func (h *Handler) InitRoutes() *gin.Engine {
 			lists.GET("/:id", h.getListById)
 			lists.PUT("/:id", h.updateList)
 			lists.DELETE("/:id", h.deleteList)
-		}
 
-		items := lists.Group("/")
-		{
-			items.POST("/", h.createItem)
-			items.GET("/", h.getAllItems)
-			items.GET("/:item_id", h.getItemById)
-			items.PUT("/:item_id", h.updateItem)
-			items.DELETE("/:item_id", h.deleteItem)
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:item_id", h.getItemById)
+				items.PUT("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
+			}
 		}
 	}
 
