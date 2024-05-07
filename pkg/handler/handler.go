0db6a1d69231824aa6589435fe7d460bcfc09c3f
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-07 12:41:39 +0300

    Added methods for endpoints in handler routes

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -11,28 +11,28 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	auth := router.Group("/auth")
 	{
-		auth.POST("/sing-up")
-		auth.POST("/sing-in")
+		auth.POST("/sing-up", h.singUp)
+		auth.POST("/sing-in", h.singIn)
 	}
 
 	api := router.Group("/api")
 	{
 		lists := api.Group("/lists")
 		{
-			lists.POST("/")
-			lists.GET("/")
-			lists.GET("/:id")
-			lists.PUT("/:id")
-			lists.DELETE("/:id")
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
 		}
 
 		items := lists.Group("/")
 		{
-			items.POST("/")
-			items.GET("/")
-			items.GET("/:item_id")
-			items.PUT("/:item_id")
-			items.DELETE("/:item_id")
+			items.POST("/", h.createItem)
+			items.GET("/", h.getAllItems)
+			items.GET("/:item_id", h.getItemById)
+			items.PUT("/:item_id", h.updateItem)
+			items.DELETE("/:item_id", h.deleteItem)
 		}
 	}
 
