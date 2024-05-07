8d6d86806f34c452e6df073a33f13a317d03a775
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-07 12:32:17 +0300

    Added handlers as server param

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"TODOList_REST"
+	"TODOList_REST/pkg/handler"
 	"log"
 )
 
 func main() {
+	// используем хэдлер из нашего покета pkg который реализован из фреймворка gin
+	handlers := new(handler.Handler)
+
 	srv := new(TODOList_REST.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running server: %s", err.Error())
 	}
 }
