40a488af42a935069d12214163b9e66ea3fa7269
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-17 17:21:16 +0300

    Added config.yml and changes which uses it in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 	"TODOList_REST/pkg/handler"
 	"TODOList_REST/pkg/repository"
 	"TODOList_REST/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
@@ -17,7 +22,13 @@ func main() {
 	//handlers := new(handler.Handler)
 
 	srv := new(TODOList_REST.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
