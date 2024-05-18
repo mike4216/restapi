3fddb474998f2da780147a4d0c9ada393fe0f000
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-18 16:20:08 +0300

    Added config.yml and logic for it

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq" // Импортируем драйвер для PostgreSQL
+	"github.com/spf13/viper"
+	"log"
+	"os"
+
 	"TODOList_REST"
 	"TODOList_REST/pkg/handler"
 	"TODOList_REST/pkg/repository"
 	"TODOList_REST/pkg/service"
-	"github.com/spf13/viper"
-	"log"
 )
 
 func main() {
@@ -14,16 +18,30 @@ func main() {
 		log.Fatalf("Error initializing config %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("Error initializing DB %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	//// используем хэдлер из нашего покета pkg который реализован из фреймворка gin
-	//handlers := new(handler.Handler)
-
 	srv := new(TODOList_REST.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("Error occured while running server: %s", err.Error())
+		log.Fatalf("Error occurred while running server: %s", err.Error())
 	}
 }
 
