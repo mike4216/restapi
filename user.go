136eb0faede862b740c7ac05be9b31e03a82c86b
Michael Leshchenko <m.lestchenko@gmail.com>
2024-05-28 13:23:01 +0300

    Added sing in logic

--- user.go
+++ user.go
@@ -1,7 +1,7 @@
 package TODOList_REST
 
 type User struct {
-	Id       int    `json:"_"`
+	Id       int    `json:"_" db:"id"`
 	Name     string `json:"name" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
